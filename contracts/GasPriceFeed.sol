bbd3984785dcdc98db5205aea7566082ea9b2951
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-09 22:34:21 +0000

    Update and rename ChainlinkGasPriceFeed.sol to GasPriceFeed.sol

--- contracts/GasPriceFeed.sol
+++ contracts/GasPriceFeed.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.6.7;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
-contract ChainlinkGasPriceFeed {
+contract GasPriceFeed {
 
     AggregatorV3Interface internal gasPriceFeed;  
 
