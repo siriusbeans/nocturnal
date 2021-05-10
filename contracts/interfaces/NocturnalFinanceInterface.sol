e52ffca4f030a5ff55fd686c7b92dae1b3524ca5
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-10 02:24:36 +0000

    Update NocturnalFinanceInterface.sol

--- contracts/interfaces/NocturnalFinanceInterface.sol
+++ contracts/interfaces/NocturnalFinanceInterface.sol
@@ -4,9 +4,11 @@ pragma solidity 0.6.6;
 interface NocturnalFinanceInterface {
     function noctAddress() external view returns (address);
     function oracleAddress() external view returns (address);
-    function oracleTitle() external view returns (string);
-    function oracleToken0Address() external view returns (address);
-    function oracleToken1Address() external view returns (address);
-    function oraclePoolFee() external view returns (uint256);
-    function oracleIndexCounter() external view returns (uint256);
+    function pickWinnerAddress() external view returns (address);
+    function rewardsAddress() external view returns (address);
+    function limitOrdersAddress() external view returns (address);
+    function poolsAddress() external view returns (address);
+    function gasPriceFeedAddress() external view returns (address);
+    function setLotteryVolume() external view returns (uint256);
+    function setLotteryBatchSize() external view returns (uint256);
 }
