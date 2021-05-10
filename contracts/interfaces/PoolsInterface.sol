754d41e16ae4251208aa746c7cc783415a49da8c
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-10 21:18:27 +0000

    Delete PoolsInterface.sol

--- contracts/interfaces/PoolsInterface.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.6.6;
-
-interface PoolsInterface {
-    function poolTitle(uint256) external view returns (string);
-    function poolToken0Address(uint256) external view returns (address);
-    function poolToken1Address(uint256) external view returns (address);
-    function poolFee(uint256) external view returns (uint256);
-}
