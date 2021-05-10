3ea280973465fb5c826d73e376027a2238103ac0
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-10 23:04:36 +0000

    Update OrderFactory.sol

--- contracts/OrderFactory.sol
+++ contracts/OrderFactory.sol
@@ -3,6 +3,8 @@ pragma solidity 0.6.12;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.1.0/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 import {NocturnalFinanceInterface} from "./Interfaces/NocturnalFinanceInterface.sol";
 import {NoctInterface} from "./Interfaces/NoctInterface.sol";
 
@@ -14,26 +16,50 @@ import {NoctInterface} from "./Interfaces/NoctInterface.sol";
 // mapping to track volume ?  
 // need volume tracked within contracts for lotto start require statement
 
-
-contract LimitOrders is Ownable {
+contract LimitOrders is ERC721, Ownable {
     using SafeMath for uint256;
+    using Counters for Counters.Counter;
     
-    constructor() public {
+    uint256 public orderCounter;
+    Counter.counters public orderCounter;
+    
+    mapping(uint256 => address) swapPoolAddress;
+    mapping(uint256 => address) swapFromTokenAddress;
+    mapping(uint256 => address) swapToTokenAddress;
+    mapping(uint256 => uint256) swapFromTokenBalance;
+    mapping(uint256 => uint256) swapToLimitPrice;
+    mapping(uint256 => uint256) swapSlippage;
+    mapping(uint256 => uint256) swapSettlementFee;
+    
+    constructor() public ERC721 ("Nocturnal Order", "oNOCT") {
     }
     
-    function setSwapLimit() public {
-    
+    function createLimitOrder(
+            address _swapPoolAddress, 
+            address _swapFromTokenAddress, 
+            address _swapToTokenAddress, 
+            uint256 _swapFromTokenBalance, 
+            uint256 _swapToLimitPrice, 
+            uint256 _swapSlippage, 
+            uint256 _swapsettlementFee) public returns (uint256) {
+        
+        orderCounter.increment();
+        uint256 orderID = orderCounter.current();
+        
+        swapPoolAddress[orderID] = _swapPoolAddress;
+        swapFromTokenAddress[orderID] = _swapFromTokenAddress;
+        swapToTokenAddress[orderID] = _swapToTokenAddress;
+        swapFromTokenBalance[orderID] = _swapFromTokenBalance;
+        swapToLimitPrice[orderID] = _swapToLimitPrice;
+        swapSlippage[orderID] = _swapSlippage;
+        swapSettlementFee[orderID] = _swapsettlementFee;
+        
+        _mint(msg.sender, tokenID);
+        
+        return(orderID);
     }
     
-    function depositTokens() public {
-    
-    }
-    
-    function withdrawDepositedTokens() public {
-    
-    }
-    
-    function withdrawSwappedTokens() public {
+    function closeLimitOrder() public {
     
     }
 }
