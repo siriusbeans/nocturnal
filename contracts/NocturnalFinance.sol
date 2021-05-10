5dc712921e07d741a84aee51dc627ecfb61edafe
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-10 21:20:00 +0000

    Update NocturnalFinance.sol

--- contracts/NocturnalFinance.sol
+++ contracts/NocturnalFinance.sol
@@ -9,9 +9,7 @@ contract NocturnalFinance is Ownable {
     address internal oracleAddress;
     address internal pickWinnerAddress;
     address internal rewardsAddress;
-    address internal limitOrdersAddress;
-    address internal poolsAddress;
-    address internal gasPriceFeedAddress;
+    address internal orderFactoryAddress;
     
     uint256 internal volumeThreshold;
     uint256 internal batchMax;
@@ -24,23 +22,17 @@ contract NocturnalFinance is Ownable {
             address _oracleAddress, 
             address _pickWinnerAddress,
             address _rewardsAddress,
-            address _limitOrdersAddress,
-            address _poolsAddress,
-            address _gasPriceAddress) external onlyOwner { 
+            address _orderFactoryAddress) external onlyOwner { 
         require(_noctAddress != address(0));
         require(_oracleAddress != address(0));
         require(_pickWinnerAddress != address(0));
         require(_rewardsAddress != address(0));
-        require(_limitOrdersAddress != address(0));
-        require(_poolsAddress != address(0));
-        require(_gasPriceAddress != address(0));
+        require(_orderFactoryAddress != address(0));
         noctAddress = _noctAddress;
         oracleAddress = _oracleAddress;
         pickWinnerAddress = _pickWinnerAddress;
         rewardsAddress = _rewardsAddress;
-        limitOrdersAddress = _limitOrdersAddress;
-        poolsAddress = _poolsAddress;
-        gasPriceFeedAddress = _gasPriceAddress;
+        orderFactoryAddress = _limitOrdersAddress;
     }
     
     function setLotteryThreshold(uint256 _volumeThreshold) external onlyOwner {
