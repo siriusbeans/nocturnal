d9d2a210da89dbe11f829243c9ff3026d7986243
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-11 01:05:05 +0000

    Update OrderFactory.sol

--- contracts/OrderFactory.sol
+++ contracts/OrderFactory.sol
@@ -7,6 +7,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 import {NocturnalFinanceInterface} from "./Interfaces/NocturnalFinanceInterface.sol";
 import {NoctInterface} from "./Interfaces/NoctInterface.sol";
+import {OracleInterface} from "./Interfaces/OracleInterface.sol";
 
 // NEXT:
 // this must be written so that a single wallet can set multiple limit orders simultaneously
@@ -27,11 +28,18 @@ contract LimitOrders is ERC721, Ownable {
     mapping(uint256 => address) swapFromTokenAddress;
     mapping(uint256 => address) swapToTokenAddress;
     mapping(uint256 => uint256) swapFromTokenBalance;
-    mapping(uint256 => uint256) swapToLimitPrice;
+    mapping(uint256 => uint256) swapToTokenLimitPrice;
     mapping(uint256 => uint256) swapSlippage;
     mapping(uint256 => uint256) swapSettlementFee;
     
-    constructor() public ERC721 ("Nocturnal Order", "oNOCT") {
+    // may only include order Address in the events...
+    event orderCreated(address _orderAddress, address _fromTokenAddress, uint256 _fromTokenBalance, address _toTokenAddress, uint256 _settlementFee);
+    event orderClosed(address _orderAddress, address _toTokenAddress, uint256 _toTokenBalance, address _fromTokenAddress, uint256 _settlementFee);
+    
+    NocturnalFinanceInterface public nocturnalFinance;
+    
+    constructor(address _nocturnalFinance) public {
+        nocturnalFinance = NocturnalFinanceInterface(_nocturnalFinance);
     }
     
     function createLimitOrder(
@@ -39,10 +47,12 @@ contract LimitOrders is ERC721, Ownable {
             address _swapFromTokenAddress, 
             address _swapToTokenAddress, 
             uint256 _swapFromTokenBalance, 
-            uint256 _swapToLimitPrice, 
+            uint256 _swapLimitPrice,
+            bool _swapAbove,
             uint256 _swapSlippage, 
-            uint256 _swapsettlementFee) public returns (uint256) {
+            uint256 _swapSettlementFee) public {
         
+        ERC721 nocturnalOrder = new ERC721 ("Nocturnal Order", "oNOCT"); 
         orderCounter.increment();
         uint256 orderID = orderCounter.current();
         
@@ -50,16 +60,41 @@ contract LimitOrders is ERC721, Ownable {
         swapFromTokenAddress[orderID] = _swapFromTokenAddress;
         swapToTokenAddress[orderID] = _swapToTokenAddress;
         swapFromTokenBalance[orderID] = _swapFromTokenBalance;
-        swapToLimitPrice[orderID] = _swapToLimitPrice;
+        swapLimitPrice[orderID] = _swapLimitPrice;
+        swapAbove[orderID] = _swapAbove[orderID]
         swapSlippage[orderID] = _swapSlippage;
-        swapSettlementFee[orderID] = _swapsettlementFee;
+        swapSettlementFee[orderID] = _swapSettlementFee;
         
-        _mint(msg.sender, tokenID);
+        _mint(msg.sender, orderID);
         
-        return(orderID);
+        // send "swap from" tokens to the ERC721 address
+        // deduct nocturnal fee % from deposited tokens and send to nocturnal rewards contract
+        
+        emit orderCreated(address(nocturnalOrder));
     }
     
-    function settleLimitOrder(uint256 _orderID, ) public {
+    function closeLimitOrder(uint256 _orderID) public {
+        // compare order attributes and price oracle result and require limit is met
+        address poolAddress = swapPoolAddress[orderID];
+        address fromTokenAddress = swapFromTokenAddress[orderID];
+        address toTokenAddress = swapToTokenAddress[orderID];
+        uint256 fromTokenBalance = swapFromTokenBalance[orderID];
+        uint256 limitPrice = swapLimitPrice[orderID];
+        uint256 above = swapAbove[orderID];
+        uint256 slippage = swapSlippage[orderID];
+        uint256 settlementFee = swapSettlementFee[orderID];
+        uint256 currentPrice = OracleInterface(nocturnalFinance.oracleAddress()).getCurrentPrice(poolAddress);
         
+        if (swapAbove == true) {
+            require(limitPrice >= currentPrice);
+        } else if (swapAbove == false) {
+            require(limitPrice <= currentPrice);
+        }
+        
+        // perform the swap
+        // deduct settlement fee
+        // send settlement fee to closer
+        // calculate and distribute NOCT rewards to closer and creator
+        // burn ERC721
     }
 }
