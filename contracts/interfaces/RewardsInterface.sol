6ec9a31a6de7a1f7f774168464dd3ed39e7e537d
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-11 22:54:40 +0000

    Update RewardsInterface.sol

--- contracts/interfaces/RewardsInterface.sol
+++ contracts/interfaces/RewardsInterface.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.6;
 
+interface OrerFactoryInterface {
+    function claimRewards() external returns ();
+    function checkRewards() external view returns (uint256);
+    function calculateOrderCreatorRewards(uint256, uint256) external view returns (uint256);
+    function calculateOrderSettlerRewards() external view returns (uint256);
+    function pendingRewards() external view returns ();
+    function totalRewards() external view returns ();
+}
