96130cbc55b99393d0d16fa9a24eae7305e017f2
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-11 22:50:04 +0000

    Update NocturnalFinance.sol

--- contracts/NocturnalFinance.sol
+++ contracts/NocturnalFinance.sol
@@ -7,40 +7,27 @@ contract NocturnalFinance is Ownable {
     
     address internal noctAddress;
     address internal oracleAddress;
-    address internal pickWinnerAddress;
     address internal rewardsAddress;
     address internal orderFactoryAddress;
-    
-    uint256 internal volumeThreshold;
-    uint256 internal batchMax;
     
     constructor() public {
     }
     
-    function init(
-            address _noctAddress, 
+    function initNocturnal( 
             address _oracleAddress, 
-            address _pickWinnerAddress,
             address _rewardsAddress,
-            address _orderFactoryAddress) external onlyOwner { 
-        require(_noctAddress != address(0));
-        require(_oracleAddress != address(0));
-        require(_pickWinnerAddress != address(0));
-        require(_rewardsAddress != address(0));
-        require(_orderFactoryAddress != address(0));
-        noctAddress = _noctAddress;
+            address _orderFactoryAddress) external onlyOwner {
+		require(_oracleAddress != address(0));
+		require(_rewardsAddress != address(0));
+		require(_orderFactoryAddress != address(0));
         oracleAddress = _oracleAddress;
-        pickWinnerAddress = _pickWinnerAddress;
         rewardsAddress = _rewardsAddress;
-        orderFactoryAddress = _limitOrdersAddress;
+        orderFactoryAddress = _orderFactoryAddress;
     }
     
-    function setLotteryThreshold(uint256 _volumeThreshold) external onlyOwner {
-        volumeThreshold = _volumeThreshold ether;
-    }
-    
-    function setLotteryBatchSize(uint256 _batchMax) external onlyOwner {
-        batchMax = _batchMax;
+    function initNoct(address _noctAddress) external onlyOwner {
+        require(_noctAddress != address(0));
+        noctAddress = _noctAddress;
     }
     
 }
