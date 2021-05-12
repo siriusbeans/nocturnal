db11c9caa3fdd0cceaf26099aa58497efb0adab0
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-12 23:54:00 +0000

    Update OracleInterface.sol

--- contracts/interfaces/OracleInterface.sol
+++ contracts/interfaces/OracleInterface.sol
@@ -2,4 +2,5 @@ pragma solidity ^0.6.6;
 
 interface OracleInterface {
     function getCurrentPrice(address) external view returns (int24);
+    function getTokens(address) external view returns (address, address);
 }
