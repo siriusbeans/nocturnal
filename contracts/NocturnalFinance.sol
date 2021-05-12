431fdad0d775fdfa78863ab4d298727735ba9b13
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-12 22:36:50 +0000

    Update NocturnalFinance.sol

--- contracts/NocturnalFinance.sol
+++ contracts/NocturnalFinance.sol
@@ -1,14 +1,15 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.6.6;
+pragma solidity ^0.6.6;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract NocturnalFinance is Ownable {
     
-    address internal noctAddress;
     address internal oracleAddress;
     address internal rewardsAddress;
     address internal orderFactoryAddress;
+    address internal feeRateAdress;
+    address internal noctAddress;
+    address internal sNoctAddress;
     
     constructor() public {
     }
@@ -16,18 +17,26 @@ contract NocturnalFinance is Ownable {
     function initNocturnal( 
             address _oracleAddress, 
             address _rewardsAddress,
-            address _orderFactoryAddress) external onlyOwner {
+            address _orderFactoryAddress,
+            address _feeRateAddress) external onlyOwner {
 		require(_oracleAddress != address(0));
 		require(_rewardsAddress != address(0));
 		require(_orderFactoryAddress != address(0));
+		require(_feeRateAddress != address(0));
         oracleAddress = _oracleAddress;
         rewardsAddress = _rewardsAddress;
         orderFactoryAddress = _orderFactoryAddress;
+        feeRateAddress = _feeRateAddress;
     }
     
     function initNoct(address _noctAddress) external onlyOwner {
         require(_noctAddress != address(0));
         noctAddress = _noctAddress;
+    }
+    
+    function initsNoct(address _sNoctAddress) external onlyOwner {
+        require(_sNoctAddress != address(0));
+        sNoctAddress = _sNoctAddress;
     }
     
 }
