42af9c3337ce67b7e134f14979065223398ed181
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-12 22:39:19 +0000

    Update NoctInterface.sol

--- contracts/interfaces/NoctInterface.sol
+++ contracts/interfaces/NoctInterface.sol
@@ -1,7 +1,6 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.7.6;
+pragma solidity ^0.6.6;
 
-interface IERC20 {
+interface NoctInterface {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function transfer(address recipient, uint256 amount) external returns (bool);
