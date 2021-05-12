de006d081c25e6e4e70e5cf069c20ac339cdd003
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-12 22:44:41 +0000

    Update OrderFactoryInterface.sol

--- contracts/interfaces/OrderFactoryInterface.sol
+++ contracts/interfaces/OrderFactoryInterface.sol
@@ -1,1 +1,15 @@
+pragma solidity ^0.6.6;
 
+interface OrderFactoryInterface {
+    function createLimitOrder(
+            address _swapPoolAddress, 
+            address _swapFromTokenAddress, 
+            address _swapToTokenAddress, 
+            uint256 _swapFromTokenBalance, 
+            uint256 _swapLimitPrice,
+            bool _swapAbove,
+            uint256 _swapSlippage, 
+            uint256 _swapSettlementFee) external returns ();
+    function settleLimitOrder(address) external returns ();
+    function closeLimitOrder(address) external returns ();
+}
