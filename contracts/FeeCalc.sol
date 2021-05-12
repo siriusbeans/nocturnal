df8226390606dbf41e3ae01b9e141a5ab2d71936
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-12 23:08:25 +0000

    Update FeeCalc.sol

--- contracts/FeeCalc.sol
+++ contracts/FeeCalc.sol
@@ -1,1 +1,16 @@
+pragma solidity ^0.6.6;
 
+import "https://github.com/Uniswap/uniswap-v3-core/blob/main/contracts/interfaces/IUniswapV3Pool.sol";
+import {NocturnalFinanceInterface} from "./Interfaces/NocturnalFinanceInterface.sol";
+
+contract FeeCalc {
+    
+    constructor(address _nocturnalFinance) public {
+        nocturnalFinance = NocturnalFinanceInterface(_nocturnalFinance);
+    }
+   
+    function getDepositFee() external view returns (uint256) {
+        uint256 _fee = nocturnalFinance.setDepositRate();
+        return(_fee);
+    }
+}
