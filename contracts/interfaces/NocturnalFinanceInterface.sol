c6c8c3738985cb5ee5519bcee930efec874f20fc
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-12 22:43:48 +0000

    Update NocturnalFinanceInterface.sol

--- contracts/interfaces/NocturnalFinanceInterface.sol
+++ contracts/interfaces/NocturnalFinanceInterface.sol
@@ -1,9 +1,10 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.6.6;
+pragma solidity ^0.6.6;
 
 interface NocturnalFinanceInterface {
-    function noctAddress() external view returns (address);
     function oracleAddress() external view returns (address);
     function rewardsAddress() external view returns (address);
     function orderFactoryAddress() external view returns (address);
+    function feeRateAddress() external view returns (address);
+    function noctAddress() external view returns (address);
+    function sNoctAddress() external view returns (address);
 }
