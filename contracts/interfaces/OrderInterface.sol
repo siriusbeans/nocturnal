feb93768e575c27aebc9d5527d16daaf1eeb7c64
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-14 03:13:54 +0000

    Update OrderInterface.sol

--- contracts/interfaces/OrderInterface.sol
+++ contracts/interfaces/OrderInterface.sol
@@ -1,1 +1,9 @@
+pragma solidity ^0.6.6;
 
+interface OrderInterface {
+    function ownerOf(uint256) external view returns (address);
+    function burn(uint256) external returns ();
+    function transferOrder(address, address, uint256) external returns ();
+    function orderSwap(address, address, bool, uint256, uint160) external returns ();
+    function _mint(address, uint256) external returns ();
+}
