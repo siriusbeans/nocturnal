cdccaac1f62e95cc3633ccf573ee98695673ba89
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-15 04:17:20 +0000

    Update Noct.sol

--- contracts/Noct.sol
+++ contracts/Noct.sol
@@ -1,3 +1,13 @@
+/*                              $$\                                             $$\                                                         
+                                $$ |                                            $$ |                                                  
+$$$$$$$\   $$$$$$\   $$$$$$$\ $$$$$$\   $$\   $$\  $$$$$$\  $$$$$$$\   $$$$$$\  $$ |     
+$$  __$$\ $$  __$$\ $$  _____|\_$$  _|  $$ |  $$ |$$  __$$\ $$  __$$\  \____$$\ $$ |    
+$$ |  $$ |$$ /  $$ |$$ /        $$ |    $$ |  $$ |$$ |  \__|$$ |  $$ | $$$$$$$ |$$ |     
+$$ |  $$ |$$ |  $$ |$$ |        $$ |$$\ $$ |  $$ |$$ |      $$ |  $$ |$$  __$$ |$$ |     
+$$ |  $$ |\$$$$$$  |\$$$$$$$\   \$$$$  |\$$$$$$  |$$ |      $$ |  $$ |\$$$$$$$ |$$ |      
+\__|  \__| \______/  \_______|   \____/  \______/ \__|      \__|  \__| \_______|\__|     
+*/
+
 pragma solidity ^0.6.6;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/ERC20.sol";
@@ -8,9 +18,21 @@ contract Noct is ERC20 ("Nocturnal Token", "NOCT") {
     using SafeMath for uint256;
 
     NocturnalFinanceInterface public nocturnalFinance;
-
+   
     constructor (address _nocturnalFinance) public {
         nocturnalFinance = NocturnalFinanceInterface(_nocturnalFinance);
-        _mint(nocturnalFinance.rewardsAddress(), 22000000 * 10 ** uint256(decimals()));
+        address dev0Address = ;
+        address dev1Address = ;
+    }
+    
+    mintTesterRewards() external {
+        require(nocturnalFinance.testerAddress[msg.sender] == true, "not tester address");
+        uint256 rewards = testerRewards[msg.sender];
+        _mint(msg.sender, rewards * 10 ** uint256(decimals()));
+    }
+    
+    mintRewards(uint256 _amount, address _recipient) external {
+        require(msg.sender == nocturnalFinance.rewardsCalcAddress(), "only rewards calc contract address can mint more NOCT");
+        _mint(_recipient, _amount * 10 ** uint256(decimals()));
     }
 }
