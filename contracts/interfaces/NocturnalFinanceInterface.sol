67d54f82eb5f20d299bce9f829ea13b4e3fa9fd5
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-15 04:22:00 +0000

    Update NocturnalFinanceInterface.sol

--- contracts/interfaces/NocturnalFinanceInterface.sol
+++ contracts/interfaces/NocturnalFinanceInterface.sol
@@ -1,10 +1,25 @@
+/*                              $$\                                             $$\                                                         
+                                $$ |                                            $$ |                                                  
+$$$$$$$\   $$$$$$\   $$$$$$$\ $$$$$$\   $$\   $$\  $$$$$$\  $$$$$$$\   $$$$$$\  $$ |     
+$$  __$$\ $$  __$$\ $$  _____|\_$$  _|  $$ |  $$ |$$  __$$\ $$  __$$\  \____$$\ $$ |    
+$$ |  $$ |$$ /  $$ |$$ /        $$ |    $$ |  $$ |$$ |  \__|$$ |  $$ | $$$$$$$ |$$ |     
+$$ |  $$ |$$ |  $$ |$$ |        $$ |$$\ $$ |  $$ |$$ |      $$ |  $$ |$$  __$$ |$$ |     
+$$ |  $$ |\$$$$$$  |\$$$$$$$\   \$$$$  |\$$$$$$  |$$ |      $$ |  $$ |\$$$$$$$ |$$ |      
+\__|  \__| \______/  \_______|   \____/  \______/ \__|      \__|  \__| \_______|\__|     
+*/
+
 pragma solidity ^0.6.6;
 
 interface NocturnalFinanceInterface {
     function oracleAddress() external view returns (address);
-    function rewardsAddress() external view returns (address);
     function orderFactoryAddress() external view returns (address);
+    function orderAddress() external view returns (address);
     function feeRateAddress() external view returns (address);
     function noctAddress() external view returns (address);
     function sNoctAddress() external view returns (address);
+    
+    function depositRate() external returns ();
+    function rewardsFactor() external view returns ();
+    function testerAddress(address) external returns (bool);
+    function testerRewards() external returns (uint256);
 }
