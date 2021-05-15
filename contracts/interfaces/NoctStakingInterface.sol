bc60f63800fca156c9577ab6a05c4fb9749707b7
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-15 18:42:13 +0000

    Update NoctStakingInterface.sol

--- contracts/interfaces/NoctStakingInterface.sol
+++ contracts/interfaces/NoctStakingInterface.sol
@@ -13,6 +13,7 @@ pragma solidity ^0.8.0;
 interface NoctStakingInterface {
     function totalStaked() external view returns (uint256);
     function stake(uint256) external returns ();
+    function autoStake(uint256, address) public ();
     function withdraw(uint256) external returns ();
     function bRSLC(address) external view returns (uint256)
     function pendingETHRewards(address) external view returns (uint256)
