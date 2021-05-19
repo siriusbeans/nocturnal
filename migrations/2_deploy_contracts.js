c38abaf518c37d3be335859a4b0f04ba0591b3cb
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-19 04:02:19 +0000

    Add files via upload

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,6 +6,11 @@ const NocturnalFinance = artifacts.require("./NocturnalFinance.sol");
 const Oracle = artifacts.require("./Oracle.sol");
 const Order = artifacts.require("./Order.sol");
 const OrderFactory = artifacts.require("./OrderFactory.sol");
+const OrderCreator = artifacts.require("./OrderCreator.sol");
+const OrderSettler = artifacts.require("./OrderSettler.sol");
+const OrderCloser = artifacts.require("./OrderCloser.sol");
+const OrderModifier = artifacts.require("./OrderModifier.sol");
+const OrderTransfer = artifacts.require("./OrderTransfer.sol");
 const Rewards = artifacts.require("./Rewards.sol");
 const LinkToken = artifacts.require("./Mocks/LinkToken.sol");
 const WethToken = artifacts.require("./Mocks/WethToken.sol");
@@ -18,6 +23,11 @@ module.exports = function(deployer, network, accounts) {
     let OracleInstance;
     let OrderInstance;
     let OrderFactoryInstance;
+    let OrderCreatorInstance;
+    let OrderSettlerInstance;
+    let OrderCloserInstance;
+    let OrderModifierInstance;
+    let OrderTransferInstance;
     let RewardsInstance;
     let LinkTokenInstance;
     let WethTokenInstance;
@@ -45,11 +55,31 @@ module.exports = function(deployer, network, accounts) {
         }).then(instance => {
             NoctStakingInstance = instance;
         
-            return deployer.deploy(OrderFactory, NocturnalFinance.address, WethToken.address, { from: ownerAddress });
+            return deployer.deploy(OrderFactory, NocturnalFinance.address, { from: ownerAddress });
         }).then(instance => {
             OrderFactoryInstance = instance;
             
-            return deployer.deploy(Oracle, NocturnalFinance.address, { from: ownerAddress });
+            return deployer.deploy(OrderCreator, NocturnalFinance.address, WethToken.address, { from: ownerAddress });
+        }).then(instance => {
+            OrderCreatorInstance = instance;
+            
+            return deployer.deploy(OrderSettler, NocturnalFinance.address, WethToken.address, { from: ownerAddress });
+        }).then(instance => {
+            OrderSettlerInstance = instance;
+            
+            return deployer.deploy(OrderCloser, NocturnalFinance.address, { from: ownerAddress });
+        }).then(instance => {
+            OrderCloserInstance = instance;
+            
+            return deployer.deploy(OrderModifier, NocturnalFinance.address, { from: ownerAddress });
+        }).then(instance => {
+            OrderModifierInstance = instance;
+            
+            return deployer.deploy(OrderTransfer, NocturnalFinance.address, { from: ownerAddress });
+        }).then(instance => {
+            OrderTransferInstance = instance;
+            
+            return deployer.deploy(Oracle, { from: ownerAddress });
         }).then(instance => {
             OracleInstance = instance;
             
@@ -64,12 +94,20 @@ module.exports = function(deployer, network, accounts) {
         });
     }).then(function() {
 
-        return NocturnalFinanceInstance.initNocturnal(OracleInstance.address, RewardsInstance.address, OrderFactoryInstance.address, OrderInstance.address);
+        return NocturnalFinanceInstance.initNocturnal(OracleInstance.address, 
+                        RewardsInstance.address, 
+                        OrderFactoryInstance.address,
+                        OrderCreatorInstance.address,
+                        OrderSettlerInstance.address, 
+                        OrderCloserInstance.address, 
+                        OrderModifierInstance.address, 
+                        OrderTransferInstance.address, 
+                        OrderInstance.address);
     }).then(function() {
 
         return NocturnalFinanceInstance.initNoct(NoctInstance.address);
     }).then(function() {
 
-        return NocturnalFinanceInstance.initsNoct(sNoctInstance.address);
+        return NocturnalFinanceInstance.initsNoct(NoctStakingInstance.address);
     });
 };
