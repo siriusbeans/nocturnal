23320ff65a47f4405bcbf3a41da532a03b3d9392
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-20 22:49:59 +0000

    Add files via upload

--- contracts/interfaces/OracleInterface.sol
+++ contracts/interfaces/OracleInterface.sol
@@ -12,6 +12,6 @@ pragma solidity ^0.8.0;
 
 interface OracleInterface {
     function getCurrentPrice(address) external view returns (uint256);
-    function getPriceReciprocal(uint256) external view returns (uint256);
+    function getCurrentPriceReciprocal(address) external view returns (uint256);
     function getTokens(address) external view returns (address, address);
 }
