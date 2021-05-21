696c7b11bfcb3c5e228cfda39253b2e54db28e33
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-21 05:33:25 +0000

    Update mockPool.sol

--- MockTests/mockPool.sol
+++ MockTests/mockPool.sol
@@ -8,30 +8,22 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MockPool is Ownable {
     using SafeMath for uint256;
-    
-    uint256 public immutable MAXINT = type(uint256).max;
+
     address internal constant UniswapV3SwapRouter = 0xbBca0fFBFE60F60071630A8c80bb6253dC9D6023;
     address internal constant UniswapV3Factory = 0x7046f9311663DB8B7cf218BC7B6F3f17B0Ea1047;
-    uint256 internal constant bPDivisor = 1000;  // 100th of a bip
-    uint24 internal constant fee = 100; // 1%
     uint256 internal constant multiplicand = 1e18;
-    
-    address internal token0;
-    address internal token1;
     
     IUniswapV3Factory public factory;
     ISwapRouter public swapRouter;
     IUniswapV3Pool public pool;
     
-    constructor(address _token0, address _token1) {
-        token0 = _token0;
-        token1 = _token1;
+    constructor() {
         swapRouter = ISwapRouter(UniswapV3SwapRouter);
         factory = IUniswapV3Factory(UniswapV3Factory);
     }
     
     function createPool(address _token0, address _token1, uint24 _fee) public onlyOwner {
-        address poolAddress = factory.createPool(token0, token1, fee);
+        address poolAddress = factory.createPool(_token0, _token1, _fee);
     }
     
     function getPool(address _token0, address _token1, uint24 _fee) external view returns (address) {
@@ -51,7 +43,8 @@ contract MockPool is Ownable {
     }
     
     function getTokens(address _pool) external view returns (address, address) {
-        address token0 = IUniswapV3Pool(_pool).token0();
-        address token1 = IUniswapV3Pool(_pool).token1();
+        address _token0 = IUniswapV3Pool(_pool).token0();
+        address _token1 = IUniswapV3Pool(_pool).token1();
+        return (_token0, _token1);
     }
 }    
