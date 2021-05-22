cf01e8f494e57890982a6ea7bb72a9e401c53976
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-22 05:29:43 +0000

    Update Oracle.sol

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -18,6 +18,7 @@ contract Oracle {
     using SafeMath for uint256;
 
     uint256 internal constant multiplicand = 1e18;
+    address internal constant UniswapV3Factory = 0x7046f9311663DB8B7cf218BC7B6F3f17B0Ea1047;
 
     IUniswapV3Pool public pool;
     
@@ -43,5 +44,11 @@ contract Oracle {
         return(token0, token1);
     }
     
-    
+    function isV3(address _pool) external view returns (bool) {
+        address factory = IUniswapV3Pool(_pool).factory(); 
+        if (factory == UniswapV3Factory) {
+            return (true);
+        } else {
+            return (false);
+    }
 }
