5f2d32ce6ae3be2d6c6e243e1fbe8d42274669b7
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-05-26 03:38:32 +0000

    Add files via upload

--- contracts/Mocks/TokenMinter.sol
+++ contracts/Mocks/TokenMinter.sol
@@ -4,8 +4,16 @@ import {TokenInterface} from "./TokenInterface.sol";
 
 contract TokenMinter {
     
+    address internal LINK;
+    address internal WETH;
+    
     constructor(address _link, address _weth) {
-        TokenInterface(_link).mint(msg.sender, 1000000);
-        TokenInterface(_weth).mint(msg.sender, 1000000);
+        LINK = _link;
+        WETH = _weth;
+    }
+    
+    function mintTokens(address _link, address _weth) external {
+        TokenInterface(_link).mint(msg.sender, 1000);
+        TokenInterface(_weth).mint(msg.sender, 1000);
     }
 }  
