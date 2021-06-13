939a0c0c35d2d330a2976c531761bb1b7a46870d
siriusbeans <76192007+siriusbeans@users.noreply.github.com>
2021-06-13 19:55:16 +0000

    Add files via upload

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -16,7 +16,7 @@ const DistributeRewards = artifacts.require("./DistributeRewards.sol");
 const TokenMinter = artifacts.require("./Mocks/TokenMinter.sol");
 const TokenSwapper = artifacts.require("./Mocks/TokenSwapper.sol");
 
-module.exports = function(deployer, network, accounts) {
+module.exports = async function(deployer, network, accounts) {
     const toWei = (value) => web3.utils.toWei(value.toString(), "ether");
     const ownerAddress = accounts[0]; 
     const initialSupply = toWei(1100000);
@@ -48,66 +48,125 @@ module.exports = function(deployer, network, accounts) {
     // need to deploy each contract one at a time for testing (in separate transaction)
     // all contracts' bytecode is less than 24Kb size limit
     // but during migration, max block gas limit is exceeded
-    // i suspect that this script is deploying multiple contracts in same transaction
+    let NocturnalFinanceInstance = deployer.deploy(NocturnalFinance, { from: ownerAddress });
+    let TokenMinterInstance = deployer.deploy(TokenMinter, LINK, WETH, { from: ownerAddress });
+    let TokenSwapperInstance = deployer.deploy(TokenSwapper, { from: ownerAddress });
+    let CreateOrderInstance = deployer.deploy(CreateOrder, NocturnalFinance.address, WETH, { from: ownerAddress });
+    let NoctStakingInstance = deployer.deploy(NoctStaking, NocturnalFinance.address, WETH, { from: ownerAddress });
+    let DepositOrderInstance = deployer.deploy(DepositOrder, NocturnalFinance.address, WETH, { from: ownerAddress });  
+    let SettleOrderInstance = deployer.deploy(SettleOrder, NocturnalFinance.address, WETH, { from: ownerAddress });
+    let CloseOrderInstance = deployer.deploy(CloseOrder, NocturnalFinance.address, WETH, { from: ownerAddress });       
+    let SettlerTransferInstance = deployer.deploy(SettleOrderTransfer, NocturnalFinance.address, { from: ownerAddress });       
+    let OracleInstance = deployer.deploy(Oracle, { from: ownerAddress });
+    let OrderInstance = deployer.deploy(Order, NocturnalFinance.address, { from: ownerAddress });
+    let RewardsInstance = deployer.deploy(Rewards, NocturnalFinance.address, rewardsSupply, initialSupply, { from: ownerAddress });         
+    let TreasuryInstance = deployer.deploy(Treasury, NocturnalFinance.address, { from: ownerAddress });       
+    let DistributeRewardsInstance = deployer.deploy(DistributeRewards, NocturnalFinance.address, { from: ownerAddress });         
+    NocturnalFinanceInstance.initNocturnal(1, CreateOrderInstance.address);
+    NocturnalFinanceInstance.initNocturnal(2, DepositOrderInstance.address);
+    NocturnalFinanceInstance.initNocturnal(3, SettleOrderInstance.address);
+    NocturnalFinanceInstance.initNocturnal(4, CloseOrderInstance.address);
+    NocturnalFinanceInstance.initNocturnal(6, SettleOrderTransferInstance.address);
+    NocturnalFinanceInstance.initNocturnal(7, OracleInstance.address);
+    NocturnalFinanceInstance.initNocturnal(8, OrderInstance.address);
+    NocturnalFinanceInstance.initNocturnal(9, RewardsInstance.address);
+    NocturnalFinanceInstance.initNocturnal(10, TreasuryInstance.address);
+    NocturnalFinanceInstance.initNocturnal(11, DistributeRewardsInstance.address);
+    let NoctInstance = deployer.deploy(Noct, NocturnalFinance.address, rewardsSupply, initialSupply, { from: ownerAddress });
+    NocturnalFinanceInstance.initNocturnal(12, NoctInstance.address);
+    NocturnalFinanceInstance.initNocturnal(0, NoctStakingInstance.address);    
+    NocturnalFinanceInstance.setPlatformRate(pRate);
+    NocturnalFinanceInstance.setRewardsFactor(rFactor);
+    NocturnalFinanceInstance.setTreasuryFactor(tFactor);
+    NocturnalFinanceInstance.setOrderURI(uri); 
+    
+    /*
     deployer.then(function() {
             return deployer.deploy(NocturnalFinance, { from: ownerAddress });
         }).then(instance => {
             NocturnalFinanceInstance = instance;
             
+        });
+        
+    deployer.then(function() {
             return deployer.deploy(TokenMinter, LINK, WETH, { from: ownerAddress });
         }).then(instance => {
             TokenMinterInstance = instance;
+        });
             
+    deployer.then(function() {            
             return deployer.deploy(TokenSwapper, { from: ownerAddress });
         }).then(instance => {
             TokenSwapperInstance = instance;
-    
-            return deployer.deploy(CreateOrder, NocturnalFinance.address, WETH, { from: ownerAddress , gas: 4600000 });
+        });
+        
+    deployer.then(function() {            
+            return deployer.deploy(CreateOrder, NocturnalFinance.address, WETH, { from: ownerAddress });
         }).then(instance => {
             CreateOrderInstance = instance;
-  
+        });
+ 
+    deployer.then(function() {             
             return deployer.deploy(NoctStaking, NocturnalFinance.address, WETH, { from: ownerAddress });
         }).then(instance => {
             NoctStakingInstance = instance;
-            
+        });
+
+    deployer.then(function() {                         
             return deployer.deploy(DepositOrder, NocturnalFinance.address, WETH, { from: ownerAddress });
         }).then(instance => {
             DepositOrderInstance = instance;
-            
+        });
+         
+    deployer.then(function() {                
             return deployer.deploy(SettleOrder, NocturnalFinance.address, WETH, { from: ownerAddress });
         }).then(instance => {
             SettleOrderInstance = instance;
-            
+        });
+
+    deployer.then(function() {                  
             return deployer.deploy(CloseOrder, NocturnalFinance.address, WETH, { from: ownerAddress });
         }).then(instance => {
             CloseOrderInstance = instance;
+        });
             
+    deployer.then(function() {                  
             return deployer.deploy(SettleOrderTransfer, NocturnalFinance.address, { from: ownerAddress });
         }).then(instance => {
             SettleOrderTransferInstance = instance;
+        });
             
+    deployer.then(function() {                  
             return deployer.deploy(Oracle, { from: ownerAddress });
         }).then(instance => {
             OracleInstance = instance;
-            
+        });
+
+    deployer.then(function() {                  
             return deployer.deploy(Order, NocturnalFinance.address, { from: ownerAddress });
         }).then(instance => {
             OrderInstance = instance;
-            
+        });
+
+    deployer.then(function() {                             
             return deployer.deploy(Rewards, NocturnalFinance.address, rewardsSupply, initialSupply, { from: ownerAddress });
         }).then(instance => {
             RewardsInstance = instance; 
-               
+        });
+           
+    deployer.then(function() {                                 
             return deployer.deploy(Treasury, NocturnalFinance.address, { from: ownerAddress });
         }).then(instance => {
             TreasuryInstance = instance;  
+        });
             
+    deployer.then(function() {                                 
             return deployer.deploy(DistributeRewards, NocturnalFinance.address, { from: ownerAddress });
         }).then(instance => {
             DistributeRewardsInstance = instance;  
-            
         });
-    }).then(function() {
+            
+    deployer.then(function() {
 
         return NocturnalFinanceInstance.initNocturnal(1, CreateOrderInstance.address);
     }).then(function() {
@@ -161,4 +220,5 @@ module.exports = function(deployer, network, accounts) {
 
         return NocturnalFinanceInstance.setOrderURI(uri); 
     });
+    */
 };
